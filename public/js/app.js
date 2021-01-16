4abbac010db3f8ee773d90befeef0e0254ac6af5
Zuzana K <zk0433@gmail.com>
2021-01-16 16:00:59 +0000

    fetch and display headlines

---
+++ public/js/app.js
@@ -0,0 +1,58 @@
+const headlinesForm = document.querySelector('form')
+const search = document.querySelector('input')
+const headlinesContent = document.querySelector('.headlines')
+const loading = document.querySelector('.loading')
+
+headlinesForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    loading.textContent = 'Loading ...'
+    headlinesContent.textContent = ''
+
+    const body = JSON.stringify({
+        queryString: search.value,
+        queryContext: {
+            curations: [
+                'ARTICLES',
+                'BLOGS',
+                'PAGES',
+            ]
+        },
+        resultContext: {
+            aspects :['title','lifecycle','location','summary','editorial' ]
+        }
+    });
+
+    const headers = new Headers({
+        'Content-Type':'application/json',
+        'X-Api-Key': apiKey,
+    })
+
+    fetch('https://cors-anywhere.herokuapp.com/https://api.ft.com/content/search/v1', {
+        method: 'POST',
+        headers: headers,
+        body,
+    }).then(response => response.json())
+        .then(data => {
+            const headlines = data.results[0].results
+
+            if (data.error || headlines === undefined) {
+                loading.textContent = ''
+                headlinesContent.textContent = "Invalid search term, please try again"
+            } else {
+                loading.textContent = ''
+                headlinesContent.innerHTML = ''
+                headlines.forEach((hl) => {
+                    headlinesContent.innerHTML += `
+                        <ul>
+                            <li>
+                                 <a href="${hl.location.uri}">${hl.title.title}</a>
+                                 <p>${hl.editorial.subheading}</p>
+                                 <span>By ${hl.editorial.byline}</span>
+                            </li>
+                        </ul>
+                    `
+                })
+            }
+        })
+})
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,19 +13,21 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 app.use(express.static(publicDir))
+app.use(cors())
 hbs.registerPartials(partialsPath)
 
-app.get('', (req,res) => {
+app.get('/', (req,res) => {
     res.render('index', {
         title: 'Financial Times Headlines',
-        author: 'Zuzana Kunckova'
+        author: 'Zuzana',
+        script: true
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About this app',
-        author: 'Zuzana Kunckova'
+        author: 'Zuzana'
     })
 })
 
